d762df3acf515aee99d75b8cbc689f6099acb6f4
Erhmprez <hajirahesson@gmail.com>
2023-12-07 22:30:48 +0000

    done with day 3

---
+++ solutions/day004.py
@@ -0,0 +1,8 @@
+# Erhmprez
+#Determine if a number is even or odd
+num = int(input('Enter a number'))
+if num % 2 == 0:
+    print(num, ' is an even number')
+
+else:
+    print(num,' is an odd number')
